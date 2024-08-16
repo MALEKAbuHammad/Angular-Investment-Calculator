34c94dcdfbfe394818c4fe5498869e485c28f222
Malek AbuHammad <abuhammadmalek4@gmail.com>
2024-08-16 18:11:14 +0300

    First Commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,31 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  // @Input() results?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+
+  private investmentService = inject(InvestmentService);
+
+  // get results() {
+  //   return this.investmentService.resultsInvestment;
+  // }
+
+  // results = computed(() => this.investmentService.resultsInvestment());
+
+  results = this.investmentService.resultsInvestment.asReadonly();
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,34 @@
+import { Component, signal } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  // @Output() investment = new EventEmitter<InvestmentInput>();
+
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
+    });
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('0');
+    this.enteredDuration.set('0');
+  }
+}
